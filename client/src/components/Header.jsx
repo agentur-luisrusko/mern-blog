e00336a9d73c23b7a4b45bf00a7295d47173017d
Luis Rusko <luis.rusko@web.de>
2024-09-08 10:08:12 +0200

    chore: create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
                     <FaMoon />
                 </Button>
                 <Link to="/sign-in" className="ml-2">
-                    <Button gradientDuoTone="purpleToBlue">
+                    <Button gradientDuoTone="purpleToBlue" outline>
                         Sign In
                     </Button>
                 </Link>
